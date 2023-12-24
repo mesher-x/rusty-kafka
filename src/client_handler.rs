16fedeefcce55f6452206f88ae5441f1a9251505
Evgeniy Mecheryakov <egmerpalo@mail.ru>
2023-12-24 23:44:13 +0300

    3rd

--- src/client_handler.rs
+++ src/client_handler.rs
@@ -1,54 +1,134 @@
+use crate::message::{InitMessage, Message, RegularMessage};
+use crate::topic::{Topic, TopicManager};
 use serde_json::{Error, Value};
-use std::io::{self, Read, Write};
+use std::fmt::Debug;
+use std::io::{self, prelude::*, BufRead, BufReader, Write};
 use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
+use std::thread;
+pub enum ClientState {
+    New,
+    Publisher,
+    Subscriber,
+}
 
 pub struct ClientHandler {
     stream: TcpStream,
+    state: ClientState,
+    topic_manager: Arc<Mutex<TopicManager>>,
+    topic_name: String,
 }
 
 impl ClientHandler {
-    pub fn new(stream: TcpStream) -> Self {
-        ClientHandler { stream }
+    pub fn new(
+        stream: TcpStream,
+        topic_manager: Arc<Mutex<TopicManager>>,
+        topic_name: String,
+    ) -> Self {
+        ClientHandler {
+            stream: stream,
+            state: ClientState::New,
+            topic_manager,
+            topic_name,
+        }
     }
-
     pub fn handle(&mut self) -> io::Result<()> {
-        let mut buffer = Vec::new();
-        self.stream.read_to_end(&mut buffer)?;
-
-        match serde_json::from_slice::<Value>(&buffer) {
-            Ok(message) => {
-                if let Some(method) = message.get("method") {
-                    match method.as_str() {
-                        Some("subscribe") => self.handle_subscribe(message),
-                        Some("publish") => self.handle_publish(message),
-                        _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Unknown method")),
+        loop {
+            let mut s = String::new();
+            let mut buf_reader = BufReader::new(&mut self.stream);
+            match buf_reader.read_line(&mut s) {
+                Ok(_) => {
+                    match Message::from_json(&s) {
+                        Ok(message) => match self.state {
+                            ClientState::New => match message {
+                                Message::InitMessage(init_msg) => {
+                                    self.state = if init_msg.method == "publish" {
+                                        ClientState::Publisher
+                                    } else {
+                                        ClientState::Subscriber
+                                    };
+                                    self.handle_init_message(init_msg);
+                                }
+                                _ => {
+                                    println!("First message must be an InitMessage");
+                                }
+                            },
+                            ClientState::Publisher => match message {
+                                Message::RegularMessage(reg_msg) => {
+                                    self.handle_regular_message(reg_msg);
+                                }
+                                _ => {
+                                    println!("Publisher can only send publish messages");
+                                }
+                            },
+                            ClientState::Subscriber => {
+                                println!("Subscribers should not send messages after initial subscription");
+                            }
+                        },
+                        Err(e) => {
+                            println!("invalid json: {}", e);
+                            break Ok(()); // Breaking the loop and closing the connection
+                        }
                     }
-                } else {
-                    Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "No method in message",
-                    ))
                 }
-            }
-            Err(e) => {
-                self.send_error(&e.to_string())?;
-                Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid JSON"))
+                Err(e) => return Err(e),
             }
         }
     }
 
-    fn handle_subscribe(&self, message: Value) -> io::Result<()> {
-        println!("Handling subscribe: {:?}", message);
+    fn handle_init_message(&mut self, message: InitMessage) -> io::Result<()> {
+        match message.method.as_str() {
+            "subscribe" => self.handle_subscribe(message),
+            "publish" => self.handle_publish(message),
+            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Unknown method")),
+        }
+    }
+
+    fn handle_subscribe(&mut self, message: InitMessage) -> io::Result<()> {
+        let topic_name = message.topic.clone();
+
+        let topic = self
+            .topic_manager
+            .lock()
+            .unwrap()
+            .get_or_create_topic(message.topic)
+            .subscribe(self.stream.try_clone().expect("could not clone the stream"));
+
+        let ip = self.stream.peer_addr()?;
+        println!(
+            "For topic {} connected subscriber with ip {}",
+            topic_name, ip
+        );
+
         Ok(())
     }
 
-    fn handle_publish(&self, message: Value) -> io::Result<()> {
-        println!("Handling publish: {:?}", message);
+    fn handle_publish(&mut self, message: InitMessage) -> io::Result<()> {
+        let topic_name = message.topic.clone();
+
+        let topic = self
+            .topic_manager
+            .lock()
+            .unwrap()
+            .get_or_create_topic(message.topic);
+
+        self.topic_name = topic_name.clone();
+
+        let ip = self.stream.peer_addr()?;
+        println!(
+            "For topic {} connected publisher with ip {}",
+            topic_name, ip
+        );
+
         Ok(())
     }
 
-    fn send_error(&mut self, error_msg: &str) -> io::Result<()> {
-        let error_response = format!("{{\"error\": \"{}\"}}", error_msg);
-        self.stream.write_all(error_response.as_bytes())
+    fn handle_regular_message(&mut self, message: RegularMessage) -> io::Result<()> {
+        let topic_name = self.topic_name.clone();
+        match self.topic_manager.lock().unwrap().get_topic(topic_name) {
+            Some(topic) => topic.publish(message),
+            None => println!("such topic does not exit yet"),
+        }
+        Ok(())
     }
 }
